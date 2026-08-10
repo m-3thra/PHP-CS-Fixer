8a7a9ef7e483f94b59c55406c24b08dcdafb261b
m-3thra <hwyvv5sj2n@privaterelay.appleid.com>
2026-08-10 06:59:36 +0000

    Remove Japanese comments

--- src/Fixer/Comment/RemoveCommentsFixer.php
+++ src/Fixer/Comment/RemoveCommentsFixer.php
@@ -26,10 +26,6 @@ use PhpCsFixer\Tokenizer\Tokens;
  */
 final class RemoveCommentsFixer extends AbstractFixer
 {
-    /**
-     * このfixerが何をするものなのか定義する。
-     * 説明文やコード例などを持たせ、ドキュメント生成にも使われる。
-     */
     public function getDefinition(): FixerDefinition
     {
         return new FixerDefinition(
@@ -42,20 +38,11 @@ final class RemoveCommentsFixer extends AbstractFixer
         );
     }
 
-    /**
-     * 渡されたPHPコードが、このFixerの処理対象になり得るかを素早く判定する場所。
-     * 例：「そもそもコメントが一つも存在しないコード」を早々に除外する。
-     * このメソッドは大量に呼ばれるので、重い処理をしないことが重要。
-     */
     public function isCandidate(Tokens $tokens): bool
     {
         return $tokens->isTokenKindFound(\T_COMMENT);
     }
 
-    /**
-     * 実際にコードを書き換える本体。
-     * 最終的にはここでTokensを調べて、条件に一致するコメントを削除する。
-     */
     protected function applyFix(\SplFileInfo $file, Tokens $tokens): void
     {
         foreach ($tokens as $index => $token) {
